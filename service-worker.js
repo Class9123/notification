405f5ca04821fc98fd6c5a40b22f520bd952ce83
Class9123 <163959445+Class9123@users.noreply.github.com>
2024-08-10 07:08:24 +0545

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activated');
+});
+
+self.addEventListener('push', function(event) {
+    const options = {
+        body: event.data ? event.data.text() : 'Default body',
+        icon: 'icon.png',
+        badge: 'badge.png'
+    };
+    event.waitUntil(
+        self.registration.showNotification('Notification Title', options)
+    );
+});
